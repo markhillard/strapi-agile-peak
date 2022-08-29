3d895ded4eba143ac7fff5f1c4916265b914be58
Mark Hillard <markhillard@gmail.com>
2022-08-29 12:42:07 -0400

    Update server config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,8 @@
 module.exports = ({ env }) => ({
-  url: env('RENDER_URL')
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 10000),
+  url: env('RENDER_URL'),
+  app: {
+    keys: env.array('APP_KEYS')
+  }
 })
